7fcc261e584e9f75a0db8498ca93776acd13d5d2
Marius C <marius.ciucam@gmail.com>
2025-03-27 14:46:57 +0200

    FIX: Test

--- update/sync/base_test.go
+++ update/sync/base_test.go
@@ -5,9 +5,10 @@ import (
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/require"
+
 	storageStubs "github.com/multiversx/mx-chain-go/testscommon/storage"
 	"github.com/multiversx/mx-chain-go/update"
-	"github.com/stretchr/testify/require"
 )
 
 func TestGetDataFromStorage_NilStorageShouldErr(t *testing.T) {
@@ -57,8 +58,6 @@ func TestWaitFor_ShouldTimeout(t *testing.T) {
 }
 
 func TestWaitFor_ShouldWorkAfterTheChannelIsWrittenIn(t *testing.T) {
-	t.Parallel()
-
 	chanToUse := make(chan bool, 1)
 	go func() {
 		time.Sleep(10 * time.Millisecond)
