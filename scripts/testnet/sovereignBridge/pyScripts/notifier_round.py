013f91b539d5f44efb9a31151613337eeaf5734e
axenteoctavian <axente.octavian@gmail.com>
2025-04-17 13:26:44 +0300

    removed TODO comments

--- scripts/testnet/sovereignBridge/pyScripts/notifier_round.py
+++ scripts/testnet/sovereignBridge/pyScripts/notifier_round.py
@@ -13,7 +13,6 @@ def update_notarization_round(lines, section, identifier, round) -> []:
         if line.strip().startswith("[" + section + "]"):
             section_found = True
         if section_found and identifier in line:
-        # TODO: Here fix these scripts to work for multiple chains, Octavian A.
             line = re.sub(rf"({identifier}\s*=\s*)\d+", r"\g<1>" + str(round), line)
             section_found = False
         updated_lines.append(line)
@@ -54,7 +53,6 @@ def main():
     with open(toml_path, 'r') as file:
         lines = file.readlines()
 
-    # TODO: Here fix these scripts to work for multiple chains, Octavian A.
     updated_lines = update_notarization_round(lines, "MainChainNotarization.MVX", "StartRound",
                                               current_round + 5)
 
