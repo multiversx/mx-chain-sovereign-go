530e92e4166ca590f372aa4ba5b92798e5912dd1
Marius C <marius.ciucam@gmail.com>
2025-08-11 13:45:30 +0300

    FIX: After review

--- sharding/sovereignChainParametersHolder.go
+++ sharding/sovereignChainParametersHolder.go
@@ -10,6 +10,7 @@ type sovereignChainParametersHolder struct {
 	*chainParametersHolder
 }
 
+// NewSovereignChainParametersHolder creates a sovereign chain parameters holder
 func NewSovereignChainParametersHolder(args ArgsChainParametersHolder) (*sovereignChainParametersHolder, error) {
 	err := validateSovereignArgs(args)
 	if err != nil {
