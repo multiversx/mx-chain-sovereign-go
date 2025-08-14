6831fc7d49a422c62d149b8bfb31df59426ebc9f
Marius C <marius.ciucam@gmail.com>
2025-08-14 13:23:25 +0300

    FEAT: Unit tests + fixes + cleanup

--- consensus/spos/worker.go
+++ consensus/spos/worker.go
@@ -886,15 +886,6 @@ func (wrk *Worker) removeConsensusHeaderFromPool() {
 	if check.IfNil(header) {
 		return
 	}
-	originalHeaderhash := headerHash
-	headerHash, err := core.CalculateHash(wrk.marshalizer, wrk.hasher, header)
-	if err != nil {
-		log.Error("removeConsensusHeaderFromPool failed to calculate header hash", "err", err)
-	}
-
-	log.Error("removeConsensusHeaderFromPool calculated header hash", "headerHash", headerHash,
-		"original header Hash", originalHeaderhash,
-	)
 
 	if !wrk.enableEpochsHandler.IsFlagEnabledInEpoch(common.AndromedaFlag, header.GetEpoch()) {
 		return
@@ -906,8 +897,21 @@ func (wrk *Worker) removeConsensusHeaderFromPool() {
 		return
 	}
 
+	processedHeaderHash, err := core.CalculateHash(wrk.marshalizer, wrk.hasher, header)
+	if err != nil {
+		log.Error("removeConsensusHeaderFromPool: failed to calculate header hash", "err", err)
+		return
+	}
+
+	log.Debug("removeConsensusHeaderFromPool", "headerHash", headerHash,
+		"processedHeaderHash", processedHeaderHash,
+	)
+
 	blockProcessorWithPoolAccess.RemoveHeaderFromPool(headerHash)
 	wrk.forkDetector.RemoveHeader(header.GetNonce(), headerHash)
+
+	blockProcessorWithPoolAccess.RemoveHeaderFromPool(processedHeaderHash)
+	wrk.forkDetector.RemoveHeader(header.GetNonce(), processedHeaderHash)
 }
 
 // DisplayStatistics logs the consensus messages split on proposed headers
