048939ad004519cc8503671fbd84392d89411610
axenteoctavian <axente.octavian@gmail.com>
2025-09-01 11:12:28 +0300

    fix after merge

--- integrationTests/chainSimulator/staking/stakingProvider/delegation_test.go
+++ integrationTests/chainSimulator/staking/stakingProvider/delegation_test.go
@@ -2061,21 +2061,6 @@ func testChainSimulatorMergingDelegation(t *testing.T, cs chainSimulatorIntegrat
 	require.Equal(t, expectedTopUpValue, getBLSTopUpValue(t, metachainNode, delegationAddress))
 }
 
-func getBLSKeyOwner(t *testing.T, metachainNode chainSimulatorProcess.NodeHandler, blsKey []byte) []byte {
-	scQuery := &process.SCQuery{
-		ScAddress:  vm.StakingSCAddress,
-		FuncName:   "getOwner",
-		CallerAddr: vm.ValidatorSCAddress,
-		CallValue:  big.NewInt(0),
-		Arguments:  [][]byte{blsKey},
-	}
-	result, _, err := metachainNode.GetFacadeHandler().ExecuteSCQuery(scQuery)
-	require.Nil(t, err)
-	require.Equal(t, chainSimulatorIntegrationTests.OkReturnCode, result.ReturnCode)
-
-	return result.ReturnData[0]
-}
-
 func TestChainSimulator_CreateDelegationContractAndWithdraw(t *testing.T) {
 	if testing.Short() {
 		t.Skip("this is not a short test")
