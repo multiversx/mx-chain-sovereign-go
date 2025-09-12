bacf0a2a4115d4103eca1a16946c9a02701c6c9b
axenteoctavian <axente.octavian@gmail.com>
2025-09-12 17:52:14 +0300

    start time delay 120s if using elastic search

--- scripts/testnet/include/config.sh
+++ scripts/testnet/include/config.sh
@@ -229,7 +229,7 @@ updateNodeConfig() {
 generateStartTime() {
   local DELAY=$GENESIS_DELAY
   if [ "${USE_ELASTICSEARCH:-0}" -eq 1 ]; then
-    DELAY=$((DELAY + 60))
+    DELAY=$((DELAY + 120))
   fi
 
   if [ "$ROUND_DURATION_IN_MS" -lt 1000 ]; then
