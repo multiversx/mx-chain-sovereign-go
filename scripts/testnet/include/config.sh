4bb83cdec139c7c05482472786a2babecd841775
axenteoctavian <axente.octavian@gmail.com>
2025-09-15 16:43:58 +0300

    update delay time

--- scripts/testnet/include/config.sh
+++ scripts/testnet/include/config.sh
@@ -229,7 +229,7 @@ updateNodeConfig() {
 generateStartTime() {
   local DELAY=$GENESIS_DELAY
   if [ "${USE_ELASTICSEARCH:-0}" -eq 1 ]; then
-    DELAY=$((DELAY + 120))
+    DELAY=$((DELAY + 90))
   fi
 
   if [ "$ROUND_DURATION_IN_MS" -lt 1000 ]; then
