cec14218ffc46d2b24751a4b8a1d48a642f888bc
axenteoctavian <axente.octavian@gmail.com>
2025-09-29 17:47:26 +0300

    script update

--- scripts/testnet/sovereignBridge/config/sovereign.snippets.sh
+++ scripts/testnet/sovereignBridge/config/sovereign.snippets.sh
@@ -11,19 +11,18 @@ deployPhaseOne() {
     echo "Deploying phase one..."
 
     local HEX_CHAIN_ID=$(echo -n "$SOV_CHAIN_PREFIX" | xxd -p)
-    ARGS_FILE="$(eval echo "$PHASE_ONE_ARGS_FILE")"
     sed -e "s/\${SOV_CHAIN_PREFIX}/${HEX_CHAIN_ID}/g" \
         -e "s/\${SHARD_VALIDATORCOUNT}/${SHARD_VALIDATORCOUNT}/g" \
-        "config/deployArguments/phaseOneArgs" > "$ARGS_FILE"
+        "config/deployArguments/phaseOneArgs" > "$PHASE_ONE_ARGS_FILE"
 
-    local OUTFILE="${OUTFILE_PATH}/deploy-phase-one.interaction.json"
-    mxpy contract call "$SOVEREIGN_FORGE_ADDRESS" \
+    local OUTFILE="$OUTFILE_PATH/deploy-phase-one.interaction.json"
+    mxpy contract call $SOVEREIGN_FORGE_ADDRESS \
         --pem "$WALLET" \
         --proxy "$PROXY" \
         --gas-limit 25000000 \
-        --abi "$(eval echo "$SOVEREIGN_FORGE_ABI")" \
+        --abi "$SOVEREIGN_FORGE_ABI" \
         --function "deployPhaseOne" \
-        --arguments-file "$ARGS_FILE" \
+        --arguments-file "$PHASE_ONE_ARGS_FILE" \
         --outfile "$OUTFILE" \
         --wait-result \
         --send || return
@@ -37,8 +36,8 @@ deployPhaseOne() {
 deployPhaseTwo() {
     echo "Deploying phase two..."
 
-    local OUTFILE="${OUTFILE_PATH}/deploy-phase-two.interaction.json"
-    mxpy contract call "$SOVEREIGN_FORGE_ADDRESS" \
+    local OUTFILE="$OUTFILE_PATH/deploy-phase-two.interaction.json"
+    mxpy contract call $SOVEREIGN_FORGE_ADDRESS \
         --pem "$WALLET" \
         --proxy "$PROXY" \
         --gas-limit 30000000 \
@@ -49,15 +48,15 @@ deployPhaseTwo() {
 
     printTxStatus "$OUTFILE" || return
 
-    ESDT_SAFE_ADDRESS=$(readContractAddress "$ESDT_SAFE_INDEX")
-    echo "mvx-esdt-safe contract: ${ESDT_SAFE_ADDRESS}"
+    ESDT_SAFE_ADDRESS=$(readContractAddress $ESDT_SAFE_INDEX)
+    echo "mvx-esdt-safe contract: $ESDT_SAFE_ADDRESS"
     ESDT_SAFE_ADDRESS_SOVEREIGN=$(computeFirstSovereignContractAddress)
-    echo "sov-esdt-safe contract: ${ESDT_SAFE_ADDRESS_SOVEREIGN}"
+    echo "sov-esdt-safe contract: $ESDT_SAFE_ADDRESS_SOVEREIGN"
 
     echo "Registering native ESDT token..."
 
-    local OUTFILE="${OUTFILE_PATH}/register-native-token.interaction.json"
-    mxpy contract call "$ESDT_SAFE_ADDRESS" \
+    local OUTFILE="$OUTFILE_PATH/register-native-token.interaction.json"
+    mxpy contract call $ESDT_SAFE_ADDRESS \
         --pem "$WALLET" \
         --proxy "$PROXY" \
         --gas-limit 80000000 \
@@ -65,7 +64,7 @@ deployPhaseTwo() {
         --arguments \
             str:"$NATIVE_ESDT_TICKER" \
             str:"$NATIVE_ESDT_NAME" \
-        --value "$ESDT_ISSUE_COST" \
+        --value $ESDT_ISSUE_COST \
         --outfile "$OUTFILE" \
         --wait-result \
         --send || return
@@ -73,14 +72,14 @@ deployPhaseTwo() {
     printTxStatus "$OUTFILE" || return
 
     NATIVE_ESDT=$(readNativeESDT)
-    echo "Native ESDT Token: ${NATIVE_ESDT}"
+    echo "Native ESDT Token: $NATIVE_ESDT"
 }
 
 deployPhaseThree() {
     echo "Deploying phase three..."
 
-    local OUTFILE="${OUTFILE_PATH}/deploy-phase-three.interaction.json"
-    mxpy contract call "$SOVEREIGN_FORGE_ADDRESS" \
+    local OUTFILE="$OUTFILE_PATH/deploy-phase-three.interaction.json"
+    mxpy contract call $SOVEREIGN_FORGE_ADDRESS \
         --pem "$WALLET" \
         --proxy "$PROXY" \
         --gas-limit 30000000 \
@@ -93,17 +92,17 @@ deployPhaseThree() {
 
     printTxStatus "$OUTFILE" || return
 
-    FEE_MARKET_ADDRESS=$(readContractAddress "$FEE_MARKET_INDEX")
-    echo "mvx-fee-market contract: ${FEE_MARKET_ADDRESS}"
+    FEE_MARKET_ADDRESS=$(readContractAddress $FEE_MARKET_INDEX)
+    echo "mvx-fee-market contract: $FEE_MARKET_ADDRESS"
     FEE_MARKET_ADDRESS_SOVEREIGN=$(computeSecondSovereignContractAddress)
-    echo "sov-fee-market contract: ${FEE_MARKET_ADDRESS_SOVEREIGN}"
+    echo "sov-fee-market contract: $FEE_MARKET_ADDRESS_SOVEREIGN"
 }
 
 deployPhaseFour() {
     echo "Deploying phase four..."
 
-    local OUTFILE="${OUTFILE_PATH}/deploy-phase-four.interaction.json"
-    mxpy contract call "$SOVEREIGN_FORGE_ADDRESS" \
+    local OUTFILE="$OUTFILE_PATH/deploy-phase-four.interaction.json"
+    mxpy contract call $SOVEREIGN_FORGE_ADDRESS \
         --pem "$WALLET" \
         --proxy "$PROXY" \
         --gas-limit 25000000 \
@@ -114,19 +113,19 @@ deployPhaseFour() {
 
     printTxStatus "$OUTFILE" || return
 
-    HEADER_VERIFIER_ADDRESS=$(readContractAddress "$HEADER_VERIFIER_INDEX")
-    echo "header-verifier contract: ${HEADER_VERIFIER_ADDRESS}"
+    HEADER_VERIFIER_ADDRESS=$(readContractAddress $HEADER_VERIFIER_INDEX)
+    echo "header-verifier contract: $HEADER_VERIFIER_ADDRESS"
 }
 
 registerBLSKeys() {
     echo "Registering validator BLS keys in main chain..."
     checkVariables CHAIN_CONFIG_ADDRESS || return
 
-    BLS_PUB_KEYS=$(python3 "${SCRIPT_PATH}/pyScripts/read_bls_keys.py")
+    BLS_PUB_KEYS=$(python3 "$SCRIPT_PATH/pyScripts/read_bls_keys.py")
 
     for BLS_KEY in ${BLS_PUB_KEYS}; do
-        local OUTFILE="${OUTFILE_PATH}/register-${BLS_KEY}.interaction.json"
-        mxpy contract call "$CHAIN_CONFIG_ADDRESS" \
+        local OUTFILE="$OUTFILE_PATH/register-${BLS_KEY}.interaction.json"
+        mxpy contract call $CHAIN_CONFIG_ADDRESS \
             --pem "$WALLET" \
             --proxy "$PROXY" \
             --gas-limit 20000000 \
@@ -144,8 +143,8 @@ registerBLSKeys() {
 completeSetupPhase() {
     echo "Completing setup phase..."
 
-    local OUTFILE="${OUTFILE_PATH}/complete-setup-phase.interaction.json"
-    mxpy contract call "$SOVEREIGN_FORGE_ADDRESS" \
+    local OUTFILE="$OUTFILE_PATH/complete-setup-phase.interaction.json"
+    mxpy contract call $SOVEREIGN_FORGE_ADDRESS \
         --pem "$WALLET" \
         --proxy "$PROXY" \
         --gas-limit 50000000 \
@@ -160,11 +159,11 @@ completeSetupPhase() {
 readNativeESDT() {
     checkVariables ESDT_SAFE_ADDRESS || return
 
-    local NATIVE_ESDT_HEX=$(mxpy contract query "$ESDT_SAFE_ADDRESS" \
+    local NATIVE_ESDT_HEX=$(mxpy contract query $ESDT_SAFE_ADDRESS \
         --proxy "$PROXY" \
         --function "getNativeToken")
 
-    echo $(hexToString "$NATIVE_ESDT_HEX")
+    echo $(hexToString $NATIVE_ESDT_HEX)
 }
 
 HEADER_VERIFIER_INDEX=2
@@ -177,27 +176,27 @@ readContractAddress() {
         return 1
     fi
 
-    local DATA=$(mxpy contract query "$SOVEREIGN_FORGE_ADDRESS" \
+    local DATA=$(mxpy contract query $SOVEREIGN_FORGE_ADDRESS \
         --proxy "$PROXY" \
-        --abi "$(eval echo "$SOVEREIGN_FORGE_ABI")" \
+        --abi "$SOVEREIGN_FORGE_ABI" \
         --function "getDeployedSovereignContracts" \
         --arguments str:"$SOV_CHAIN_PREFIX")
 
-    local HEX_ADDRESS=$(echo "${DATA}" | jq -r \
+    local HEX_ADDRESS=$(echo "$DATA" | jq -r \
         --argjson id "$1" '
         .[][]
         | select(.id.__discriminant__ == $id)
         | .address
         ')
 
-    echo $(hexToBech32 "$HEX_ADDRESS")
+    echo $(hexToBech32 $HEX_ADDRESS)
 }
 
 unpauseEsdtSafeContractSovereign() {
     echo "Unpausing sovereign deposits..."
 
-    local OUTFILE="${OUTFILE_PATH}/unpause.interaction.json"
-    mxpy contract call "$ESDT_SAFE_ADDRESS_SOVEREIGN" \
+    local OUTFILE="$OUTFILE_PATH/unpause.interaction.json"
+    mxpy contract call $ESDT_SAFE_ADDRESS_SOVEREIGN \
         --pem "$WALLET" \
         --proxy "$PROXY_SOVEREIGN" \
         --gas-limit 10000000 \
