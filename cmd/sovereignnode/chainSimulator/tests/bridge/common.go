71e8749c08867f813555805de58e19b52d386728
axenteoctavian <axente.octavian@gmail.com>
2025-10-01 15:33:10 +0300

    update sovereign bridge contracts

--- cmd/sovereignnode/chainSimulator/tests/bridge/common.go
+++ cmd/sovereignnode/chainSimulator/tests/bridge/common.go
@@ -26,9 +26,8 @@ var serializer, _ = abi.NewSerializer(abi.ArgsNewSerializer{
 
 // ArgsBridgeSetup holds the arguments for bridge setup
 type ArgsBridgeSetup struct {
-	ESDTSafeAddress     []byte
-	FeeMarketAddress    []byte
-	SovRegistrarAddress []byte
+	ESDTSafeAddress  []byte
+	FeeMarketAddress []byte
 }
 
 // deploySovereignBridgeSetup will deploy all bridge contracts
@@ -57,14 +56,11 @@ func deploySovereignBridgeSetup(
 		"@00" // no fee
 	feeMarketAddress := chainSim.DeployContract(t, cs, wallet.Bytes, &nonce, systemScAddress, feeMarketArgs, feeMarketWasmPath)
 
-	sovRegistrarAddress := chainSim.DeployContract(t, cs, wallet.Bytes, &nonce, systemScAddress, "", sovRegistrarWasmPath)
-
 	chainSim.SendTransactionWithSuccess(t, cs, wallet.Bytes, &nonce, esdtSafeAddress, chainSim.ZeroValue, "unpause", uint64(10000000))
 
 	return ArgsBridgeSetup{
-		ESDTSafeAddress:     esdtSafeAddress,
-		FeeMarketAddress:    feeMarketAddress,
-		SovRegistrarAddress: sovRegistrarAddress,
+		ESDTSafeAddress:  esdtSafeAddress,
+		FeeMarketAddress: feeMarketAddress,
 	}
 }
 
