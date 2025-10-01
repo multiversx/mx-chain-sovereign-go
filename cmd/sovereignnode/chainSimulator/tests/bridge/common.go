5712b7d2bc5ae6a49b0b87ca5cafd9facde495ca
axenteoctavian <axente.octavian@gmail.com>
2025-10-01 15:44:10 +0300

    update 2 sovereign bridge contracts

--- cmd/sovereignnode/chainSimulator/tests/bridge/common.go
+++ cmd/sovereignnode/chainSimulator/tests/bridge/common.go
@@ -43,7 +43,6 @@ func deploySovereignBridgeSetup(
 	wallet dtos.WalletAddress,
 	esdtSafeWasmPath string,
 	feeMarketWasmPath string,
-	sovRegistrarWasmPath string,
 ) ArgsBridgeSetup {
 	nodeHandler := cs.GetNodeHandler(core.SovereignChainShardId)
 	systemScAddress := chainSim.GetSysAccBytesAddress(t, nodeHandler)
@@ -56,7 +55,7 @@ func deploySovereignBridgeSetup(
 		"@00" // no fee
 	feeMarketAddress := chainSim.DeployContract(t, cs, wallet.Bytes, &nonce, systemScAddress, feeMarketArgs, feeMarketWasmPath)
 
-	chainSim.SendTransactionWithSuccess(t, cs, wallet.Bytes, &nonce, esdtSafeAddress, chainSim.ZeroValue, "unpause", uint64(10000000))
+	chainSim.SendTransactionWithSuccess(t, cs, wallet.Bytes, &nonce, esdtSafeAddress, chainSim.ZeroValue, "unpause", uint64(10_000_000))
 
 	return ArgsBridgeSetup{
 		ESDTSafeAddress:  esdtSafeAddress,
