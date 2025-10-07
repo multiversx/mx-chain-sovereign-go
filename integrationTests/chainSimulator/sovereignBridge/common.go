e9262ebdbae2e7d2b3b4546788f026a23565d5db
axenteoctavian <axente.octavian@gmail.com>
2025-10-07 10:19:46 +0300

    SC execution nonce

--- integrationTests/chainSimulator/sovereignBridge/common.go
+++ integrationTests/chainSimulator/sovereignBridge/common.go
@@ -59,6 +59,7 @@ type registeredBLSKeys struct {
 
 // ArgsBridgeSetup holds the arguments for bridge setup
 type ArgsBridgeSetup struct {
+	ExecutionNonce        uint64
 	RegisteredBLSKeys     registeredBLSKeys
 	SovereignForgeAddress []byte
 	ChainConfigAddress    []byte
@@ -432,7 +433,7 @@ func executeOperation(
 	operation := hex.EncodeToString(receiver) + // receiver address
 		lengthOn4Bytes(len(bridgedInTokens)) + // nr of tokens
 		getTokenDataArgs(bridgeData.OwnerAccount.Wallet.Bytes, bridgedInTokens) + // tokens encoded arg
-		getUint64Bytes(0) + // event nonce
+		getUint64Bytes(getAndIncrementExecutionNonce(bridgeData)) + // event nonce
 		hex.EncodeToString(originalSender) + // sender address from other chain
 		getTransferDataArgs(transferData)
 	operationBytes, _ := hex.DecodeString(operation)
@@ -442,6 +443,13 @@ func executeOperation(
 		"@" + hex.EncodeToString(hashOfHashes) +
 		"@" + operation
 	return chainSim.SendTransaction(t, cs, bridgeData.OwnerAccount.Wallet.Bytes, &bridgeData.OwnerAccount.Nonce, bridgeData.ESDTSafeAddress, chainSim.ZeroValue, executeBridgeOpsData, uint64(100000000))
+}
+
+func getAndIncrementExecutionNonce(bridgeData *ArgsBridgeSetup) uint64 {
+	executionNonce := bridgeData.ExecutionNonce
+	bridgeData.ExecutionNonce += 1
+
+	return executionNonce
 }
 
 func getTokenDataArgs(creator []byte, tokens []chainSim.ArgsDepositToken) string {
@@ -500,7 +508,7 @@ func registerTokenOperation(
 		lengthOn4Bytes(len(ticker)) + // length of ticker
 		hex.EncodeToString([]byte(ticker)) + // ticker
 		"00000012" + // 18 decimals
-		getUint64Bytes(1) + // event nonce
+		getUint64Bytes(getAndIncrementExecutionNonce(bridgeData)) + // event nonce
 		hex.EncodeToString(originalSender) + // sender address from other chain
 		"00" // no transfer data
 	operationBytes, _ := hex.DecodeString(operation)
