740a69762c08300a81e2fc68a8f0efa40cf0f885
axenteoctavian <axente.octavian@gmail.com>
2025-10-17 16:29:48 +0300

    fix after merge

--- scripts/testnet/convert_address.py
+++ scripts/testnet/convert_address.py
@@ -5,7 +5,7 @@ from multiversx_sdk import Address
 
 def main():
     # input arguments
-    address = Address.from_bech32(sys.argv[1])
+    address = Address.new_from_bech32(sys.argv[1])
     new_hrp = sys.argv[2]
 
     print(Address(address.pubkey, new_hrp).to_bech32())
