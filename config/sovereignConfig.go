0da5813b152339d256aafdb23844c82ee0642392
axenteoctavian <axente.octavian@gmail.com>
2025-10-22 16:07:59 +0300

    removed NativeESDT from sovereign config

--- config/sovereignConfig.go
+++ config/sovereignConfig.go
@@ -8,7 +8,6 @@ type SovereignConfig struct {
 	OutgoingSubscribedEvents         OutgoingSubscribedEvents `toml:"OutgoingSubscribedEvents"`
 	OutGoingBridge                   OutGoingBridge           `toml:"OutGoingBridge"`
 	NotifierConfig                   NotifierConfig           `toml:"NotifierConfig"`
-	GenesisConfig                    GenesisConfig            `toml:"GenesisConfig"`
 	OutGoingBridgeCertificate        OutGoingBridgeCertificate
 }
 
@@ -62,9 +61,4 @@ type WebSocketConfig struct {
 	WithAcknowledge    bool   `toml:"WithAcknowledge"`
 	AcknowledgeTimeout int    `toml:"AcknowledgeTimeout"`
 	Version            uint32 `toml:"Version"`
-}
-
-// GenesisConfig should hold all sovereign genesis related configs
-type GenesisConfig struct {
-	NativeESDT string `toml:"NativeESDT"`
 }
